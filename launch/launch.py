ed6b2b5eb37903afe7f2bdeb126847aedc7460dc
Lohmann27 <s203876@student.dtu.dk>
2023-11-06 10:31:12 +0100

    Launch file til robotten v1 - ikke færdig

---
+++ launch/launch.py
@@ -0,0 +1,63 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='fejemis_sim' 
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()   
+    )
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )])
+    )
+ 
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+    rviz2 = Node(
+        package="rviz2",
+        namespace='',
+        executable="rviz2",
+        name="rviz2",
+        arguments=['-d' + os.path.join(get_package_share_directory('fejemis_sim'), 'config', 'main.rviz')]
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        rsp,
+        joystick,
+        diff_drive_spawner,
+        joint_broad_spawner,
+        rviz2,
+        
+    ])
